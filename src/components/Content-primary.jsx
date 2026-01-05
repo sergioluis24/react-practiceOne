14892dbd53c42b86afc446a85b2fbbeef288007c
sergioluis24 <sergioluisl2324@gmail..com>
2026-01-05 14:26:46 -0400

    Feat:Added logic for the pagination

--- src/components/Content-primary.jsx
+++ src/components/Content-primary.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { Pagination } from "./Pagination.jsx";
 
 export function ContentPrimary() {
+  const [currentPage, setCurrentPage] = useState(1);
+  function handlePageChange(page) {
+    setCurrentPage(page);
+  }
   return (
     <>
       <main className="grow shrink">
         <h1>Hola</h1>
-        <Pagination currtenPage={1} totalPages={5} />
+        <Pagination
+          currentPage={currentPage}
+          totalPages={10}
+          onPageChange={handlePageChange}
+        />
       </main>
     </>
   );
