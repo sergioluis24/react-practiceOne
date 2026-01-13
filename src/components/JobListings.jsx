2881cbb1b296fd2ace3eac248a0c01964f191c41
sergioluis24 <sergioluisl2324@gmail..com>
2026-01-13 11:58:15 -0400

    Feat:fix little bug from joblistings

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,8 @@
 import { JobCard } from "./JobCard.jsx";
-import data from "./../data.json";
-export function JobListings(job) {
+export function JobListings(jobs) {
   return (
     <section className="border boder-gray-300 rounded-xl overflow-hidden">
-      {data.map((job) => (
+      {jobs.map((job) => (
         <JobCard job={job} key={job.id} onLast={job.length - 1} />
       ))}
     </section>
